69c60a54a530c18c43093981cfe1091a0dcee9f7
Artur Moreira <arturacm@gmail.com>
2022-10-09 13:17:30 -0300

    feat: add count-inversions algorithm
    new algorithm added for counting inverions
    based on merge sort

---
+++ count-inversions.js
@@ -0,0 +1,38 @@
+function countAndMerge(A, B) {
+  if (!B) return { inversions: 0, numbers: A };
+  if (!A) return { inversions: 0, numbers: B };
+  let i = 0;
+  let j = 0;
+
+  const C = [];
+  let inversions = 0;
+  for (let k = 0; k < A.length + B.length; k++) {
+    if (A[i] < B[j] || j === B.length) {
+      C.push(A[i]);
+      i++;
+    } else {
+      if (i < A.length) {
+        inversions += A.length - i;
+      }
+      C.push(B[j]);
+      j++;
+    }
+  }
+  return { inversions, numbers: C };
+}
+
+function countAndSort(C) {
+  if (!C || C.length === 1 || C.length === 0)
+    return { inversions: 0, numbers: C };
+  const k = (C.length + (C.length % 2)) / 2;
+  const A = C.splice(0, k);
+  const { inversions: inversionsA, numbers: sortedA } = countAndSort(A);
+  const { inversions: inversionsB, numbers: sortedB } = countAndSort(C);
+  const { numbers, inversions } = countAndMerge(sortedA, sortedB);
+  return {
+    numbers,
+    inversions: inversions + inversionsA + inversionsB,
+  };
+}
+
+console.log(countAndSort([1, 3, 5, 2, 4, 6]));
